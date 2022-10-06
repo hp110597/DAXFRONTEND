9d28153cd55931e089910331b3418bebe96a0da2
Huy Phan <pqhcan@gmail.com>
2022-10-06 18:16:03 +0700

    'add-animation'

--- components/image/index.jsx
+++ components/image/index.jsx
@@ -7,23 +7,21 @@ import { Keyframes } from "scenes/img";
 
 const ImageBackground = (props) => {
   
-const animateImage=(props)=>{
-  return  <Keyframes name={props.name} from={props.startState}  to={props.endState}/>
-   
-}
+
   return (
-    <div>
+    <>
     <Image
       src={props.src}
       alt={props.alt}
       width={props.width}
       height={props.height}
+      // layout='fill'
       objectFit="contain"
       className={props.className}
       style={props.style}
-      keyframes={animateImage}
     />
-    </div>
+    </>
+    
   );
 };
 
