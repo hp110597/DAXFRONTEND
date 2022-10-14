0b2e805df1cbfb6ad8d541c2b03e53df2de63f43
NGUYEN DUY THANG <nguyenduythang811@gmail.com>
2022-10-15 02:59:49 +0700

    Fix next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack(config) {
-    config.resolve.modules.push(__dirname);
+    config.resolve.alias["components"] = path.join(__dirname, "components");
+    config.resolve.alias["hooks"] = path.join(__dirname, "hooks");
+    config.resolve.alias["pages"] = path.join(__dirname, "pages");
+    config.resolve.alias["public"] = path.join(__dirname, "public");
+    config.resolve.alias["scenes"] = path.join(__dirname, "scenes");
+    config.resolve.alias["styles"] = path.join(__dirname, "styles");
+    config.resolve.alias["utils"] = path.join(__dirname, "utils");
 
     return config;
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,102 +8,126 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
-      '3xl': '2000px',
-      '4xl': '2500px'
+      "3xl": "2000px",
+      "4xl": "2500px",
     },
     extend: {
       keyframes: {
         dialogFadeIN: {
-          '0%': { transform: 'translateY(-25%)', opacity: '0.8' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+          "0%": { transform: "translateY(-25%)", opacity: "0.8" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
         },
         dotActiveFadein: {
-          '0%': { transform: 'scale(0.8)', opacity: '0.3' },
-          '100%': { transform: 'scale(1)', opacity: '1' },
+          "0%": { transform: "scale(0.8)", opacity: "0.3" },
+          "100%": { transform: "scale(1)", opacity: "1" },
         },
         scroll: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(var(--percentage))' },
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(var(--percentage))" },
         },
         imageShowUp: {
-          '0%': { opacity: '0', transform: 'translateX(var(--startX)) translateY(var(--startY))' },
-          '100%': { opacity: '1', transform: 'translateX(var(--endX))  translateY(var(--endY))' },
+          "0%": {
+            opacity: "0",
+            transform: "translateX(var(--startX)) translateY(var(--startY))",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(var(--endX))  translateY(var(--endY))",
+          },
         },
         textAnimate: {
-          '0%': {
-            opacity: '0.5',
-            transform: 'translateY(var(--startPosition))'
+          "0%": {
+            opacity: "0.5",
+            transform: "translateY(var(--startPosition))",
           },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(var(--endPosition))'
-          }
+          "100%": {
+            opacity: "1",
+            transform: "translateY(var(--endPosition))",
+          },
         },
         imageFadeIn: {
-          '0%': { opacity: 'var(--startOpacity)', transform: 'translateX(var(--startX)) translateY(var(--startY))' },
-          '100%': { opacity: 'var(--endOpacity)', transform: 'translateX(var(--endX))  translateY(var(--endY))' },
+          "0%": {
+            opacity: "var(--startOpacity)",
+            transform: "translateX(var(--startX)) translateY(var(--startY))",
+          },
+          "100%": {
+            opacity: "var(--endOpacity)",
+            transform: "translateX(var(--endX))  translateY(var(--endY))",
+          },
         },
         textAnimateFadeOut: {
-          '0%': { opacity: '1', transform: 'translateY(var(--startPosition))' },
-          '100%': { opacity: '0', transform: 'translateY(var(--endPosition))' },
+          "0%": { opacity: "1", transform: "translateY(var(--startPosition))" },
+          "100%": { opacity: "0", transform: "translateY(var(--endPosition))" },
         },
         imageFadeOut: {
-          '0%': { opacity: '1', transform: 'translateX(var(--startX)) translateY(var(--startY))', visibility: 'visible' },
-          '100%': { opacity: '0', transform: 'translateX(var(--endX))  translateY(var(--endY))', visibility: 'hidden' }
+          "0%": {
+            opacity: "1",
+            transform: "translateX(var(--startX)) translateY(var(--startY))",
+            visibility: "visible",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "translateX(var(--endX))  translateY(var(--endY))",
+            visibility: "hidden",
+          },
         },
         fadeIn: {
-          '0%': { transform: 'translateX(50%)', opacity: '0.5' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+          "0%": { transform: "translateX(50%)", opacity: "0.5" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
         },
         fadeOut: {
-          '0%': { visibility: 'visible' },
-          '100%': { visibility: 'hidden' },
+          "0%": { visibility: "visible" },
+          "100%": { visibility: "hidden" },
         },
         process: {
-          '0%': { width: '0px' },
-          '100%': { width: '100%' },
-        }
+          "0%": { width: "0px" },
+          "100%": { width: "100%" },
+        },
       },
       animation: {
-        dialogFadeIN: 'dialogFadeIN 1s ease',
-        dotActiveFadein: 'dotActiveFadein 1s ease',
-        textScroll: 'scroll 10s linear',
-        textAnimate: 'textAnimate var(--duration) ease-in-out var(--delay) forwards',
-        imageFadeIn: 'imageFadeIn var(--duration) ease-in-out var(--delay) forwards',
-        textAnimateFadeOut: 'textAnimateFadeOut var(--duration) ease-in-out var(--delay) forwards',
-        imageFadeOut: 'imageFadeOut var(--duration) ease-in-out var(--delay) forwards',
-        fadeIn: 'fadeIn 0.7s ease-in-out',
-        fadeOut: 'fadeOut .7s ease-in-out',
-        process: 'process 3s ease-in-out forwards',
-        imageShowUp: 'imageShowUp 3s forwards',
+        dialogFadeIN: "dialogFadeIN 1s ease",
+        dotActiveFadein: "dotActiveFadein 1s ease",
+        textScroll: "scroll 10s linear",
+        textAnimate:
+          "textAnimate var(--duration) ease-in-out var(--delay) forwards",
+        imageFadeIn:
+          "imageFadeIn var(--duration) ease-in-out var(--delay) forwards",
+        textAnimateFadeOut:
+          "textAnimateFadeOut var(--duration) ease-in-out var(--delay) forwards",
+        imageFadeOut:
+          "imageFadeOut var(--duration) ease-in-out var(--delay) forwards",
+        fadeIn: "fadeIn 0.7s ease-in-out",
+        fadeOut: "fadeOut .7s ease-in-out",
+        process: "process 3s ease-in-out forwards",
+        imageShowUp: "imageShowUp 3s forwards",
       },
       fontFamily: {
-        sans: 'Averta,sans-serif',
-        avertaBold: 'AvertaBold',
-        storyBook: 'StorybookRegular'
+        sans: "Averta,sans-serif",
+        avertaBold: "AvertaBold",
+        storyBook: "StorybookRegular",
       },
       colors: {
-        'primary': '#86CFF2',
-        'bgPrimary': `#000000`
+        primary: "#86CFF2",
+        bgPrimary: `#000000`,
       },
       zIndex: {
-        '1': '1',
+        1: "1",
       },
     },
   },
   plugins: [],
-}
+};
