0f35de0a0f0f209c3c9f016cbc9a5a7b24a88411
NGUYEN DUY THANG <nguyenduythang811@gmail.com>
2022-10-15 01:23:11 +0700

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    config.resolve.modules.push(path.resolve("./"));
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
