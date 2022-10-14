8a7a320151d1ecca885a360ef2c6c4138ddffeab
NGUYEN DUY THANG <nguyenduythang811@gmail.com>
2022-10-15 02:01:18 +0700

    Fix next config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
-    config.resolve.modules.push(__dirname);
-    return config;
-  },
 };
 
 module.exports = nextConfig;
