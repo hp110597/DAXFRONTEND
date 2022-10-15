8832e1bd16d0179a28de34914d4debe818081993
NGUYEN DUY THANG <nguyenduythang811@gmail.com>
2022-10-15 10:33:13 +0700

    Fix next config

--- next.config.js
+++ next.config.js
@@ -7,18 +7,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack(config) {
-    config.resolve.alias["components"] = path.join(__dirname, "components");
-    config.resolve.alias["hooks"] = path.join(__dirname, "hooks");
-    config.resolve.alias["pages"] = path.join(__dirname, "pages");
-    config.resolve.alias["public"] = path.join(__dirname, "public");
-    config.resolve.alias["scenes"] = path.join(__dirname, "scenes");
-    config.resolve.alias["styles"] = path.join(__dirname, "styles");
-    config.resolve.alias["utils"] = path.join(__dirname, "utils");
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
 
-    return config;
-  },
-  webpackFinal: async (config, { configType }) => {
-    config.resolve.modules.push(__dirname);
+    config.resolve.alias["components"] = path.join(__dirname, "./components");
+    config.resolve.alias["hooks"] = path.join(__dirname, "./hooks");
+    config.resolve.alias["pages"] = path.join(__dirname, "./pages");
+    config.resolve.alias["public"] = path.join(__dirname, "./public");
+    config.resolve.alias["scenes"] = path.join(__dirname, "./scenes");
+    config.resolve.alias["styles"] = path.join(__dirname, "./styles");
+    config.resolve.alias["utils"] = path.join(__dirname, "./utils");
     return config;
   },
 };
