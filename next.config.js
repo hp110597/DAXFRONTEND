f4fbc6a89d19d98a2ab04cf21a6f71f36669e1ad
NGUYEN DUY THANG <nguyenduythang811@gmail.com>
2022-10-15 10:50:35 +0700

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
-    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
-
-    config.resolve.alias["components"] = path.join(__dirname, "./components");
-    config.resolve.alias["hooks"] = path.join(__dirname, "./hooks");
-    config.resolve.alias["pages"] = path.join(__dirname, "./pages");
-    config.resolve.alias["public"] = path.join(__dirname, "./public");
-    config.resolve.alias["scenes"] = path.join(__dirname, "./scenes");
-    config.resolve.alias["styles"] = path.join(__dirname, "./styles");
-    config.resolve.alias["utils"] = path.join(__dirname, "./utils");
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
