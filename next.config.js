e82d704ab8c930378723047165ab2cfa742cc4a9
hp110597 <pqhcan@gmail.com>
2022-10-16 23:02:08 +0700

    fix-config

--- next.config.js
+++ next.config.js
@@ -4,38 +4,39 @@ const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
-    config.resolve.alias["components"] = path.resolve(
-      __dirname,
-      "./components"
-    );
-    config.resolve.alias["hooks"] = path.resolve(
-      __dirname,
-      "../dax-frontend/hooks"
-    );
-    config.resolve.alias["pages"] = path.resolve(
-      __dirname,
-      "../dax-frontend/pages"
-    );
-    config.resolve.alias["public"] = path.resolve(
-      __dirname,
-      "../dax-frontend/public"
-    );
-    config.resolve.alias["scenes"] = path.resolve(
-      __dirname,
-      "../dax-frontend/scenes"
-    );
-    config.resolve.alias["styles"] = path.resolve(
-      __dirname,
-      "../dax-frontend/styles"
-    );
-    config.resolve.alias["utils"] = path.resolve(
-      __dirname,
-      "../dax-frontend/utils"
-    );
+  // webpack(config) {
+  //   config.resolve.alias["components"] = path.resolve(
+  //     __dirname,
+  //     "./components"
+  //   );
+  //   config.resolve.alias["hooks"] = path.resolve(
+  //     __dirname,
+  //     "../dax-frontend/hooks"
+  //   );
+  //   config.resolve.alias["pages"] = path.resolve(
+  //     __dirname,
+  //     "../dax-frontend/pages"
+  //   );
+  //   config.resolve.alias["public"] = path.resolve(
+  //     __dirname,
+  //     "../dax-frontend/public"
+  //   );
+  //   config.resolve.alias["scenes"] = path.resolve(
+  //     __dirname,
+  //     "../dax-frontend/scenes"
+  //   );
+  //   config.resolve.alias["styles"] = path.resolve(
+  //     __dirname,
+  //     "../dax-frontend/styles"
+  //   );
+  //   config.resolve.alias["utils"] = path.resolve(
+  //     __dirname,
+  //     "../dax-frontend/utils"
+  //   );
+    
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
